e4efb662dcd96d1d7c3a546fec38000c70b495b3
Angel Mitchell <angel.mitchell@bison.howard.edu>
2018-02-11 18:11:43 -0500

    new view

---
+++ tumblr/tumblr/PhotoDetailsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoDetailsViewController.swift
+//  tumblr
+//
+//  Created by Angel Chara'e Mitchell on 2/11/18.
+//  Copyright © 2018 Angel Chara'e Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailsViewController: UIViewController {
+
+    @IBOutlet weak var details: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
